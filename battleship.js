47e7a2a5130751cacf7b389421d75ea47476bbe8
TeamD2-GP TeamD2-GP <TeamD2-GP@GaryPedretti.com>
2024-05-23 14:31:18 +0000

    added functionality for testing so that player ships are defaulted positions

--- battleship.js
+++ battleship.js
@@ -134,18 +134,37 @@ class Battleship {
     InitializeMyFleet() {
         this.myFleet = gameController.InitializeShips();
 
-        console.log("Please position your fleet (Game board size is from A to H and 1 to 8) :");
+        const defaultPositions = {
+            "Carrier": ["A1", "A2", "A3", "A4", "A5"],
+            "Battleship": ["B1", "B2", "B3", "B4"],
+            "Cruiser": ["C1", "C2", "C3"],
+            "Submarine": ["D1", "D2", "D3"],
+            "Destroyer": ["E1", "E2"]
+        };
 
         this.myFleet.forEach(function (ship) {
-            console.log();
-            console.log(`Please enter the positions for the ${ship.name} (size: ${ship.size})`);
-            for (var i = 1; i < ship.size + 1; i++) {
-                    console.log(`Enter position ${i} of ${ship.size} (i.e A3):`);
-                    const position = readline.question();
-                    telemetryWorker.postMessage({eventName: 'Player_PlaceShipPosition', properties:  {Position: position, Ship: ship.name, PositionInShip: i}});
+
+            const positions = defaultPositions[ship.name];
+            if (positions && positions.length === ship.size) {
+                for (var i = 0; i < ship.size; i++) {
+                    const position = positions[i];
+                    telemetryWorker.postMessage({ eventName: 'Player_PlaceShipPosition', properties: { Position: position, Ship: ship.name, PositionInShip: i + 1 } });
                     ship.addPosition(Battleship.ParsePosition(position));
+                }
             }
-        })
+        });
+        // console.log("Please position your fleet (Game board size is from A to H and 1 to 8) :");
+
+        // this.myFleet.forEach(function (ship) {
+        //     console.log();
+        //     console.log(`Please enter the positions for the ${ship.name} (size: ${ship.size})`);
+        //     for (var i = 1; i < ship.size + 1; i++) {
+        //             console.log(`Enter position ${i} of ${ship.size} (i.e A3):`);
+        //             const position = readline.question();
+        //             telemetryWorker.postMessage({eventName: 'Player_PlaceShipPosition', properties:  {Position: position, Ship: ship.name, PositionInShip: i}});
+        //             ship.addPosition(Battleship.ParsePosition(position));
+        //     }
+        // })
     }
 
     InitializeEnemyFleet() {
